e9297a736e5b6346d7066e2485c6302cf7d11c68
m4aguilar <mario4.1990@gmail.com>
2017-02-24 11:12:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.prueba;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonS = (Button) findViewById(R.id.buttonStart);
+        buttonS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button buttonA = (Button) findViewById(R.id.buttonAcceder);
+        buttonA.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(v.getContext(), MapsActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+}
